968e092ca0447a181878b0481b1584fef760fa0c
Alfredo <fredogm05@gmail.com>
2021-08-18 17:11:06 -0400

    se aplico una instalacion de un paquete de Router

--- src/shared/sidebar/sidebar.component.tsx
+++ src/shared/sidebar/sidebar.component.tsx
@@ -1,37 +1,51 @@
-import { router } from "../../widgets/Router.component"
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const Sidebar = () => {
 
     return (
-<aside>
-  <div id="sidebar" className="nav-collapse ">
-
-    <ul className="sidebar-menu">
-      <li className="active">
-        <a href="index.html">
-          <i className="icon_house_alt" />
-          <span>Dashboard</span>
-        </a>
-      </li>
-      
-      <li>
-      <a href="/#" onClick={() => router('widgets')}>
-          <i className="icon_genius" />
-          <span>Widgets</span>
-        </a>
-      </li>
-      <li>
-        <a href="/#" onClick={() => router('charts')}>
-          <i className="icon_piechart" />
-          <span>Charts</span>
-        </a>
-      </li>
-    
-    </ul>
-  </div>
-</aside>
-    )
+      <Router>
+        <aside>
+          <div id="sidebar" className="nav-collapse ">
+            <ul className="sidebar-menu">
+              <li className="active">
+                <a href="index.html">
+                  <i className="icon_house_alt" />
+                  <span>Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a href="widgets.html">
+                  <i className="icon_genius" />
+                  <span>Widgets</span>
+                </a>
+              </li>
+              <li>
+                <a href="Charts.component.tsx">
+                  <i className="icon_piechart" />
+                  <span>Charts</span>
+                </a>
+              </li>
+              
+              <li className="sub-menu">
+                <a href="/" >
+                  <i className="icon_documents_alt" />
+                  <span>Pages</span>
+                  <span className="menu-arrow arrow_carrot-right" />
+                </a>
+                <ul className="sub">
+                  <li><a href="blank.html">Blank Page</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        </Router>
+            )
 }
 
 export{
